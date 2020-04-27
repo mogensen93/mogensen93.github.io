ea69da49b15eb91fa337c9f1bc40147f4403ff83
Rasmus Mogensen <rasmusmogensen2@hotmail.com>
2020-04-27 10:22:12 +0200

    auto-commit

---
+++ weatherapp/precache-manifest.7ecce42f9d185815ddf67b949803a807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c27de052447874a45d6683b58f20cb6",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "00ef0dcb5f5f96d4f410",
+    "url": "/weatherapp/static/css/main.5f812eb3.chunk.css"
+  },
+  {
+    "revision": "0e74f2882b436ae4a126",
+    "url": "/weatherapp/static/js/2.a867763f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherapp/static/js/2.a867763f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ef0dcb5f5f96d4f410",
+    "url": "/weatherapp/static/js/main.357b672a.chunk.js"
+  },
+  {
+    "revision": "d55fb8e63572c199b149",
+    "url": "/weatherapp/static/js/runtime-main.07c5fb50.js"
+  }
+]);
